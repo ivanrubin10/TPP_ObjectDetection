7ef83811f145a3ed56f5904a934a35efe43d5ea9
Mariano Guglieri <mguglieri@fi.uba.ar>
2024-04-13 18:14:04 -0300

    modificado utils para hacerlo generico a culaquier folder path

--- utils/create_csv.py
+++ utils/create_csv.py
@@ -2,35 +2,51 @@
 # Based off code from GitHub user datitran: https://github.com/datitran/raccoon_dataset/blob/master/xml_to_csv.py
 
 import os
+import sys
 import glob
 import pandas as pd
 import xml.etree.ElementTree as ET
 
 def xml_to_csv(path):
-    xml_list = []
-    for xml_file in glob.glob(path + '/*.xml'):
-        tree = ET.parse(xml_file)
-        root = tree.getroot()
-        for member in root.findall('object'):
-            value = (root.find('filename').text,
-                     int(root.find('size')[0].text),
-                     int(root.find('size')[1].text),
-                     member[0].text,
-                     int(member[4][0].text),
-                     int(member[4][1].text),
-                     int(member[4][2].text),
-                     int(member[4][3].text)
-                     )
-            xml_list.append(value)
-    column_name = ['filename', 'width', 'height', 'class', 'xmin', 'ymin', 'xmax', 'ymax']
-    xml_df = pd.DataFrame(xml_list, columns=column_name)
-    return xml_df
+    try:
+        xml_list = []
+        for xml_file in glob.glob(path + '/*.xml'):
+            tree = ET.parse(xml_file)
+            root = tree.getroot()
+            filename = root.find('filename').text
+            size = root.find('size')
+            width = int(size.find('width').text)
+            height = int(size.find('height').text)
+            for member in root.findall('object'):
+                class_name = member[0].text
+                xmin = int(member.find('bndbox/xmin').text)
+                ymin = int(member.find('bndbox/ymin').text)
+                xmax = int(member.find('bndbox/xmax').text)
+                ymax = int(member.find('bndbox/ymax').text)
+                xml_list.append((filename, width, height, class_name, xmin, ymin, xmax, ymax))
+        
+        column_name = ['filename', 'width', 'height', 'class', 'xmin', 'ymin', 'xmax', 'ymax']
+        xml_df = pd.DataFrame(xml_list, columns=column_name)
+        return xml_df
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+        return None
 
 def main():
-    for folder in ['train','validation']:
-        image_path = os.path.join(os.getcwd(), ('images/' + folder))
+
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <path>")
+        return
+
+    # Get the path argument
+    pato = sys.argv[1]
+
+    if os.path.exists(pato) and os.path.isdir(pato):
+        image_path = os.path.join(os.getcwd(), (pato))
         xml_df = xml_to_csv(image_path)
-        xml_df.to_csv(('images/' + folder + '_labels.csv'), index=None)
+        xml_df.to_csv((pato + '_labels.csv'), index=None)
         print('Successfully converted xml to csv.')
+    else:
+        print("Folder does not exist.")
 
 main()
